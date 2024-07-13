8ac00d0d551d09a7fc3136636e71096d44a4785c
Chris Sauve <chrismsauve@gmail.com>
2024-07-13 02:38:53 -0400

    Add vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [
+    preact(),
+    VitePWA({
+      minify: false,
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+    }),
+  ],
+  build: {
+    minify: false,
+  },
+});
